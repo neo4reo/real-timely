466317c6593e4f2abe3562151b02442f5c2fda02
Nick McCrea <nick@nickmccrea.com>
2022-05-20 14:30:57 -0400

    Implement some test functions for testing out the sequencer.

---
+++ src/test_functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "test_functions.h"
+
+void print_beans()
+{
+  printf("BEANS!\n");
+}
+
+void print_cornbread()
+{
+  printf("CORNBREAD!\n");
+}
+
+void print_pickles()
+{
+  printf("PICKLES\n");
+}
---
+++ src/test_functions.h
@@ -0,0 +1,8 @@
+#ifndef TEST_FUNCTIONS_H
+#define TEST_FUNCTIONS_H
+
+void print_beans();
+void print_cornbread();
+void print_pickles();
+
+#endif
