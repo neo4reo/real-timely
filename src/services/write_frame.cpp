fd8304128b9424a6296a7eb9aedcd766de614568
Nick McCrea <nick@nickmccrea.com>
2022-06-02 17:13:11 -0400

    Fix indentation in `write_frame.cpp`.

--- src/services/write_frame.cpp
+++ src/services/write_frame.cpp
@@ -41,33 +41,33 @@ void write_frame_teardown(FramePipeline *frame_pipeline)
  */
 void write_frame(FramePipeline *frame_pipeline)
 {
-    // Dequeue the next selected frame buffer.
-    cv::Mat *frame_buffer;
-    attempt(
-        mq_receive(
-            frame_pipeline->captured_frame_queue,
-            (char *)&frame_buffer,
-            sizeof(cv::Mat *),
-            NULL),
-        "mq_receive() captured_frame_queue");
+  // Dequeue the next selected frame buffer.
+  cv::Mat *frame_buffer;
+  attempt(
+      mq_receive(
+          frame_pipeline->captured_frame_queue,
+          (char *)&frame_buffer,
+          sizeof(cv::Mat *),
+          NULL),
+      "mq_receive() captured_frame_queue");
 
-    // Write the frame to disk.
-    filename_number.str("");
-    filename_number.clear();
-    filename_number << std::right << std::setfill('0') << std::setw(6) << frame_number;
-    cv::imwrite(
-        FILENAME_PREFIX + filename_number.str() + FILENAME_EXTENSION,
-        *frame_buffer);
+  // Write the frame to disk.
+  filename_number.str("");
+  filename_number.clear();
+  filename_number << std::right << std::setfill('0') << std::setw(6) << frame_number;
+  cv::imwrite(
+      FILENAME_PREFIX + filename_number.str() + FILENAME_EXTENSION,
+      *frame_buffer);
 
-    // Increment the frame number.
-    ++frame_number;
+  // Increment the frame number.
+  ++frame_number;
 
-    // Enqueue the newly-available frame buffer.
-    attempt(
-        mq_send(
-            frame_pipeline->available_frame_queue,
-            (const char *)&frame_buffer,
-            sizeof(cv::Mat *),
-            0),
-        "mq_send() available_frame_queue");
+  // Enqueue the newly-available frame buffer.
+  attempt(
+      mq_send(
+          frame_pipeline->available_frame_queue,
+          (const char *)&frame_buffer,
+          sizeof(cv::Mat *),
+          0),
+      "mq_send() available_frame_queue");
 }
